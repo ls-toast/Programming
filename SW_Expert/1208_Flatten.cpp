11524454cb35743c6756ac8a5df3b4db79cbab78
ls-toast <leesak2004@gmail.com>
2019-08-04 23:09:53 +0900

    Add 1208

---
+++ SW_Expert/1208_Flatten.cpp
@@ -0,0 +1,44 @@
+/*
+ * SW Expert Academy 1208 Flatten
+ *
+ * Memory : 12,636 Kb
+ * RunTime : 55ms
+ *
+ * URL : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14QpAaAAwCFAYi&categoryId=AV14QpAaAAwCFAYi
+ * */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int cnt;
+vector <int> height;
+
+int dump()
+{
+    while(cnt--)
+    {
+        sort(height.begin(), height.end());
+        height[0]++;
+        height[99]--;
+    }
+    return height[98] - height[1];
+}
+
+int main()
+{
+    int t,i,j;
+    int box[100];
+    for(t = 1; t <= 10; t ++)
+    {
+        scanf("%d",&cnt);
+        for(i = 0 ; i < 100; i ++)
+        {
+            scanf("%d",&box[i]);
+            height.push_back((box[i]));
+        }
+        printf("#%d %d\n",t,dump());
+        height.clear();
+    }
+}
