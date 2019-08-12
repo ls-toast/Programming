2b7d9d0d78a37f2153e8d0359639ab8872f9330b
ls-toast <leesak2004@gmail.com>
2019-08-12 17:35:12 +0900

    Improve Run-Time

--- SW_Expert/1208_Flatten.cpp
+++ SW_Expert/1208_Flatten.cpp
@@ -2,44 +2,58 @@
  * SW Expert Academy 1208 Flatten
  *
  * Difficulty : D3
- * Memory : 12,636 Kb
- * RunTime : 55ms
+ * Memory : 12,516 Kb
+ * RunTime : 6ms
  *
  * URL : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV139KOaABgCFAYh
  * */
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int cnt;
-vector <int> height;
-
-int dump()
-{
-    while(cnt--)
-    {
-        sort(height.begin(), height.end());
-        height[0]++;
-        height[99]--;
-    }
-    return height[98] - height[1];
-}
-
+#include <stdio.h>
+#include <cstring>
 int main()
 {
-    int t,i,j;
-    int box[100];
-    for(t = 1; t <= 10; t ++)
+    int arr[101] = { 0, };
+    int i, cnt, input;
+    int tc = 0;
+    while (tc != 10)
     {
-        scanf("%d",&cnt);
-        for(i = 0 ; i < 100; i ++)
+        int max = 0;
+        int min = 101;
+        scanf("%d", &cnt);
+        for (i = 0; i < 100; i++)
         {
-            scanf("%d",&box[i]);
-            height.push_back((box[i]));
+            scanf("%d", &input);
+            if (input > max)
+                max = input;
+            if (input < min)
+                min = input;
+            arr[input]++;
         }
-        printf("#%d %d\n",t,dump());
-        height.clear();
+        while (cnt--)
+        {
+            if (arr[max] - 1 == 0)
+            {
+                arr[max]--;
+                max--;
+                arr[max]++;
+            }
+            else
+            {
+                arr[max]--;
+                arr[max - 1]++;
+            }
+            if (arr[min] - 1 == 0)
+            {
+                arr[min]--;
+                min++;
+                arr[min]++;
+            }
+            else
+            {
+                arr[min]--;
+                arr[min + 1]++;
+            }
+        }
+        printf("#%d %d\n", ++tc, max - min);
+        memset(arr, 0, sizeof(arr));
     }
 }
