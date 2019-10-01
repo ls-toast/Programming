f120c244efd68646ac25db0130084ff9615b13f7
ls-toast <leesak2004@gmail.com>
2019-10-01 22:17:13 +0900

    Add lab_soving

---
+++ Baekjoon/Samsung/14502_lab.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+int n,m;
+int arr[8][8];
+int ans;
+bool visit[8][8];
+queue <pair<int,int> > Q;
+
+void spread()
+{
+    int cnt = 0;
+    int i,j;
+    int tarr[8][8];
+    int nx[4] = {0,0,-1,1};
+    int ny[4] = {-1,1,0,0};
+    int y,x;
+    queue <pair<int,int> > tQ;
+    tQ = Q;
+    memcpy(tarr,arr,sizeof(arr));
+    while(tQ.size())
+    {
+        y = tQ.front().first;
+        x = tQ.front().second;
+        if(visit[y][x])
+        {
+            tQ.pop();
+            continue;
+        }
+        else
+        {
+            for(i = 0 ; i < 4; i ++)
+            {
+                int tmpy = y + ny[i];
+                int tmpx = x + nx[i];
+                if(tmpy >= n || tmpy < 0 || tmpx >= m || tmpx < 0)
+                    continue;
+                else if(tarr[tmpy][tmpx] == 0 && visit[tmpy][tmpx] == 0)
+                {
+                    y = tmpy;
+                    x = tmpx;
+                    tQ.push(make_pair(y,x));
+                    visit[y][x] = 1;
+                    tarr[y][x] = 2;
+                }
+            }
+        }
+    }
+    for(i = 0 ; i < n ; i ++)
+    {
+        for(j = 0 ; j < m; j ++)
+        {
+            if(tarr[i][j] == 0)
+                cnt ++;
+        }
+    }
+    max(ans,cnt);
+}
+
+void wall(int cnt)
+{
+    int i,j;
+    if(cnt == 3)
+    {
+        spread();
+        memset(visit,0,sizeof(visit));
+        return;
+    }
+    for(i = 0; i < n; i ++)
+    {
+        for(j = 0; j < m; j ++)
+        {
+            if(arr[i][j] == 0)
+            {
+                arr[i][j] = 1;
+                wall(cnt+1);
+                arr[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int i,j;
+    cin >> n >> m;
+    for(i = 0 ; i < n ; i ++)
+    {
+        for(j = 0 ; j < m; j ++)
+        {
+            cin >> arr[i][j];
+            if(arr[i][j] == 2) {
+                Q.push(make_pair(i, j));
+            }
+        }
+    }
+    wall(0);
+    //cout << ans << endl;
+}
