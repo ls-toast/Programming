9f9ddfbbc984403fb6b584c9d6947ce89f6b3219
ls-toast <leesak2004@gmail.com>
2019-10-03 20:15:28 +0900

    solved

--- Programmers/lv3_change.cpp
+++ Programmers/lv3_change.cpp
@@ -1,9 +1,37 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+int dp[1000001];
 int solution(int n, vector<int> money) {
     int answer = 0;
+    int i,j;
+    dp[0] = 1;
+    for(i = 1; i <= n; i ++)
+    {
+        if(dp[i] % money[0] == 0)
+            dp[i] = 1;
+    }
+    for(i = 1; i < money.size(); i ++)
+    {
+        for(j = money[i]; j <= n; j ++)
+        {
+            dp[j] += dp[j - money[i]];
+        }
+    }
+    answer = dp[n] % 1000000007;
     return answer;
+}
+
+int main()
+{
+    int n;
+    vector <int> arr;
+    cin >> n;
+    arr.emplace_back(1);
+    arr.emplace_back(2);
+    arr.emplace_back(5);
+    cout << solution(n, arr) << endl;
 }
