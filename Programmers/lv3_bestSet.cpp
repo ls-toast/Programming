944f9193b5741ed1f8d92dfc74620dc33960d693
ls-toast <leesak2004@gmail.com>
2019-10-05 20:05:29 +0900

    Fix Problem

--- Programmers/lv3_bestSet.cpp
+++ Programmers/lv3_bestSet.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 
@@ -7,28 +8,19 @@ using namespace std;
 vector<int> solution(int n, int s) {
     vector<int> answer;
     int i,j;
-    if(s < 2) {
+    if(n > s) {
         answer.push_back(-1);
         return answer;
     }
+    
     for(i = 0 ; i < n; i ++)
         answer.push_back(s/n);
+    
     int sz = answer.size();
     for(j = 1; j <= s%n; j ++) {
-        if(sz - j < 0)
-            j = 1;
         answer[sz - j]++;
+        if(sz - j < 0)
+            j = 0;
     }
     return answer;
-}
-
-int main()
-{
-    int n, s;
-    vector<int> ans;
-    cin >> n >> s;
-    ans = solution(n,s);
-    for(int i = 0 ; i < ans.size(); i ++)
-        cout << ans[i] << " ";
-    cout << endl;
 }
