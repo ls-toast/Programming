de9f8f9fe01e3ecb61c1b354693173fd04a5187e
ls-toast <leesak2004@gmail.com>
2019-10-14 12:45:06 +0900

    SOLV

--- Programmers/lv3_far.cpp
+++ Programmers/lv3_far.cpp
@@ -5,16 +5,14 @@ using namespace std;
 
 long long solution(int n) {
     long long answer = 0;
-    int arr[2001] = {0,1,2,};
+    long long arr[2001] = {0,1,2,};
     if(n == 1 || n == 2)
         answer = n;
     else
-    {
         for(int i = 3 ; i <= n; i ++ )
-        {
-            arr[i] += arr[i-1] + arr[i-2];
-        }
-    }
+            arr[i] = (arr[i-1] + arr[i-2])%1234567;
+    
     answer = arr[n];
     return answer;
 }
+
