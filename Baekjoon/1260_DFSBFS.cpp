d9a8355fe7b98d9710c9738ff0a97aae8e317187
ls-toast <leesak2004@gmail.com>
2019-10-22 21:54:24 +0900

    Improve

--- Baekjoon/1260_DFSBFS.cpp
+++ Baekjoon/1260_DFSBFS.cpp
@@ -1,86 +1,72 @@
 /*
  * Beakjoon 1260 DFS&BFS
  *
- * Memory : 99672 KB
- * Run-Time : 12ms
+ * Memory : 2124 KB
+ * Run-Time : 4ms
  *
  * URL : https://www.acmicpc.net/problem/1260
  * */
-
 #include <iostream>
-#include <deque>
 #include <vector>
-#include <algorithm>
+#include <queue>
 #include <cstring>
+#include <algorithm>
 using namespace std;
-bool matrix[10001][10001];
-bool visit[10001];
+bool chk[1001];
 vector <int> stk;
-deque <int> que;
+queue <int> q;
+vector <vector<int> > adj(1001);
 
-void dfs(int st, int n)
+void bfs(int stp)
 {
-    bool flag;
-    int i,cur;
-    cur = st;
-    stk.emplace_back(cur);
-    visit[cur] = 1;
-    cout << cur << " ";
-    while(!stk.empty()) {
-        flag = false;
-        for (i = 1; i <= n; i++) {
-            if (matrix[cur][i] != 0 && visit[i] != 1) {
-                cur = i;
-                stk.emplace_back(cur);
-                visit[cur] = 1;
-                flag = true;
-                cout << cur << " ";
-                i = 0;
+    q.push(stp);
+    chk[stp] = true;
+    while(!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+        cout << front << " ";
+        for(int i = 0 ; i < adj[front].size(); i ++)
+        {
+            int tmp = adj[front][i];
+            if(!chk[tmp]) {
+                q.push(tmp);
+                chk[tmp] = true;
             }
-        }
-        if(!flag) {
-            stk.pop_back();
-            cur = stk.back();
         }
     }
     cout << endl;
 }
 
-void bfs(int st, int n)
+void dfs(int stp)
 {
-    bool flag;
-    int i, cur;
-    que.emplace_back(st);
-    while(!que.empty())
-    {
-        flag = false;
-        cur = que.front();
-        que.pop_front();
-        cout << cur << " ";
-        visit[cur] = 1;
-        for(i = 1; i <= n; i ++)
-        {
-            if(matrix[cur][i] != 0 && visit[i] != 1) {
-                que.emplace_back(i);
-                visit[i] = 1;
-            }
-        }
+    stk.push_back(stp);
+    chk[stp] = true;
+    cout << stp << " ";
+    for(int i = 0 ; i < adj[stp].size(); i ++) {
+        int tmp = adj[stp][i];
+        if (!chk[tmp])
+            dfs(tmp);
     }
 }
 
 int main()
 {
-    int nNode, nEdge;
-    int stNode, aN, bN;
+    int n, m, v;
+    int vs, ve;
     int i,j;
-    cin >> nNode >> nEdge >> stNode;
-    for(i = 1 ; i <= nEdge; i ++)
+    cin >> n >> m >> v;
+    for(i = 0 ; i < m ; i ++)
     {
-        cin >> aN >> bN;
-        matrix[aN][bN] = 1;
-        matrix[bN][aN] = 1;
+        cin >> vs >> ve;
+        adj[vs].push_back(ve);
+        adj[ve].push_back(vs);
     }
-    dfs(stNode, nNode);
-    memset(visit, 0, sizeof(visit));
-    bfs(stNode, nNode);
+    for(i = 0; i < n; i ++)
+        sort(adj[i].begin(), adj[i].end());
+    dfs(v);
+    cout << endl;
+    memset(chk,false,sizeof(chk));
+    bfs(v);
+    return 0;
 }
