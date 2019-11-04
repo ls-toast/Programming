51a6a30f244e9a37e4e58cd196924cb116276701
ls-toast <leesak2004@gmail.com>
2019-11-04 14:36:26 +0900

    Rename & Add new solved prob

---
+++ Programmers/lv2/124.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    while(n)
+    {
+        if(n%3 == 0)
+        {
+            answer.insert(0,"4");
+            n = n/3-1;
+        }
+        else
+        {
+            answer.insert(0,to_string(n%3));
+            n = n/3;
+        }
+    }
+    return answer;
+}
