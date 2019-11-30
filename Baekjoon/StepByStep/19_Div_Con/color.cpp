7d7500f35c6d6acdd6b98c305b3fe56c485cf9b5
ls-toast <leesak2004@gmail.com>
2019-11-30 20:18:41 +0900

    Improve

--- Baekjoon/StepByStep/19_Div_Con/color.cpp
+++ Baekjoon/StepByStep/19_Div_Con/color.cpp
@@ -6,28 +6,32 @@ int blue;
 int white;
 
 void divide(int x_st, int y_st, int x_ed, int y_ed) {
-	int cntB = 0;
-	int cntW = 0;
-	for (int i = y_st; i <= y_ed; i++) {
-		for (int j = x_st; j <= x_ed; j++) {
-			if (map[i][j] == 1) 
-                cntB++;
-			else 
-                cntW++;
+    bool flag = true;
+	for (int i = y_st; i < y_ed; i++) {
+		for (int j = x_st; j < x_ed; j++) {
+			if (map[i][j] != map[y_st][x_st])
+            {
+               flag = false;
+               break; 
+            }
 		}
+        if(!flag)
+            break;
 	}
 
-	if (cntB == 0) { 
-		white++; 
-	}
-	else if (cntW == 0) { 
-		blue++;
-	}
-	else {
-		divide(x_st, y_st, (x_st+x_ed)/2, (y_st+y_ed)/2); //1
-		divide((x_st+x_ed)/2+1, y_st, x_ed, (y_st+y_ed)/2); //2
-		divide(x_st, (y_st+y_ed)/2+1, (x_st+x_ed)/2, y_ed); //3
-		divide((x_st+x_ed)/2+1, (y_st+y_ed)/2+1, x_ed, y_ed); //4
+    if(flag)
+    {
+        if(map[y_st][x_st] == 0)
+            white++;
+        else
+            blue++;
+    }
+	else 
+    {
+		divide(x_st, y_st, (x_st+x_ed)/2, (y_st+y_ed)/2); 
+		divide((x_st+x_ed)/2, y_st, x_ed, (y_st+y_ed)/2); 
+		divide(x_st, (y_st+y_ed)/2, (x_st+x_ed)/2, y_ed); 
+		divide((x_st+x_ed)/2, (y_st+y_ed)/2, x_ed, y_ed); 
 	}
 	return;
 }
@@ -43,6 +47,7 @@ int main() {
 		}
 	}
 
-	divide(0, 0, n-1, n-1);
+	divide(0, 0, n, n);
 	cout << white << endl << blue;
 }
+
