9264dcdd60b2eaf59dfecb9d842c1f6c104ac23b
ls-toast <46123108+ls-toast@users.noreply.github.com>
2020-03-20 16:05:24 +0900

    Add new solved prob

---
+++ Programmers/lv2/find_p_y.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = True
+    pcnt=0
+    ycnt=0
+    pcnt += s.count('p')
+    pcnt += s.count('P')
+    ycnt += s.count('y')
+    ycnt += s.count('Y')
+    if pcnt == ycnt:
+        return True
+    else:
+        return False
---
+++ Programmers/lv2/string_desc.py
@@ -0,0 +1,4 @@
+def solution(s):
+    b = ''
+    b = b.join(sorted(s,reverse=True))
+    return b
